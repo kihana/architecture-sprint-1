19e9463000e5f64117ea188dcb01c1d6cf41d321
Egor Zayats <yahor.zayats@atotech.com>
2024-12-29 21:25:55 +0100

    Removed comments.

--- frontend/host/src/components/App.js
+++ frontend/host/src/components/App.js
@@ -1,7 +1,5 @@
-//import React from "react";
 import React, { lazy, Suspense }  from "react";
 import { Route, useHistory, Switch } from "react-router-dom";
-//import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
 import PopupWithForm from "./PopupWithForm";
@@ -11,11 +9,8 @@ import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import EditProfilePopup from "./EditProfilePopup";
 import EditAvatarPopup from "./EditAvatarPopup";
 import AddPlacePopup from "./AddPlacePopup";
-//import Register from "./Register";
-//import Login from "./Login";
 import InfoTooltip from "./InfoTooltip";
 import ProtectedRoute from "./ProtectedRoute";
-//import * as auth from "../utils/auth.js";
 
 const Login = lazy(() => import('users/Login')
 //.catch(() => {
@@ -54,8 +49,6 @@ function App() {
   //В компоненты добавлены новые стейт-переменные: email — в компонент App
   const [email, setEmail] = React.useState("");
 
-//  const history = useHistory();
-
   // Запрос к API за информацией о пользователе и массиве карточек выполняется единожды, при монтировании.
   React.useEffect(() => {
     api
@@ -66,25 +59,7 @@ function App() {
       })
       .catch((err) => console.log(err));
   }, []);
-/*
-  // при монтировании App описан эффект, проверяющий наличие токена и его валидности
-  React.useEffect(() => {
-    const token = localStorage.getItem("jwt");
-    if (token) {
-      auth
-        .checkToken(token)
-        .then((res) => {
-          setEmail(res.data.email);
-          setIsLoggedIn(true);
-          history.push("/");
-        })
-        .catch((err) => {
-          localStorage.removeItem("jwt");
-          console.log(err);
-        });
-    }
-  }, [history]);
-*/
+
   function handleEditProfileClick() {
     setIsEditProfilePopupOpen(true);
   }
@@ -159,43 +134,7 @@ function App() {
       })
       .catch((err) => console.log(err));
   }
-/*
-  function onRegister({ email, password }) {
-    auth
-      .register(email, password)
-      .then((res) => {
-        setTooltipStatus("success");
-        setIsInfoToolTipOpen(true);
-        history.push("/signin");
-      })
-      .catch((err) => {
-        setTooltipStatus("fail");
-        setIsInfoToolTipOpen(true);
-      });
-  }
 
-  function onLogin({ email, password }) {
-    auth
-      .login(email, password)
-      .then((res) => {
-        setIsLoggedIn(true);
-        setEmail(email);
-        history.push("/");
-      })
-      .catch((err) => {
-        setTooltipStatus("fail");
-        setIsInfoToolTipOpen(true);
-      });
-  }
-
-  function onSignOut() {
-    // при вызове обработчика onSignOut происходит удаление jwt
-    localStorage.removeItem("jwt");
-    setIsLoggedIn(false);
-    // После успешного вызова обработчика onSignOut происходит редирект на /signin
-    history.push("/signin");
-  }
-*/
   return (
     // В компонент App внедрён контекст через CurrentUserContext.Provider
     <CurrentUserContext.Provider value={currentUser}>
@@ -246,13 +185,11 @@ function App() {
           onClose={closeAllPopups}
         />
         <ImagePopup card={selectedCard} onClose={closeAllPopups} />
-        <Suspense fallback={<div>Loading...</div>}>
           <InfoTooltip
             isOpen={isInfoToolTipOpen}
             onClose={closeAllPopups}
             status={tooltipStatus}
           />
-        </Suspense>
       </div>
     </CurrentUserContext.Provider>
   );
